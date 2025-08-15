9ece10a9be3497fd68f8aa9e7ab9c3e140196164
smsalim931 <smsalim931@gmail.com>
2025-08-16 01:03:16 +0530

    first commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({data, reference}) => {
       dragConstraints={reference}
       whileDrag={{scale:1.5}}
       dragElastic={0.1}
-      dragTransition={{bounceStiffness:100, bounceDamping:2}}
+      dragTransition={{bounceStiffness:100, bounceDamping:3}}
       className='relative flex-shrink-0 w-60 h-70 rounded-[45px] px-8 py-10 bg-zinc-600 text-amber-50 p-5 overflow-hidden'>
       <FaRegFileAlt />
       <p className='text-sm mt-5 font-semibold leading-tight '>{data.desc}</p>
